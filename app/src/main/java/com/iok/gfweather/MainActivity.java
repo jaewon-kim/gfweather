b5eee08dab51d59668421ba39cbd96e3f72bd50b
jaewon-kim <crismas@gmail.com>
2016-07-11 18:03:18 +0900

    Sync Google Drive API, Select Folder meta info

--- app/src/main/java/com/iok/gfweather/MainActivity.java
+++ app/src/main/java/com/iok/gfweather/MainActivity.java
@@ -11,13 +11,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveFolder;
 import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.DriveResource;
+import com.google.android.gms.drive.Metadata;
 import com.google.android.gms.drive.OpenFileActivityBuilder;
 import com.iok.gfweather.service.MyService;
 import com.iok.gfweather.util.SharedPrefUtil;
@@ -35,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements
     private static final String TAG = "MainActivity";
     private static String EXISTING_FOLDER_ID = "";
 
+    TextView mTvSelectedFolderName;
+
 
     /**
      * Request code for auto Google Play Services error resolution.
@@ -46,9 +54,12 @@ public class MainActivity extends AppCompatActivity implements
      */
     protected static final int NEXT_AVAILABLE_REQUEST_CODE = 2;
 
+    private static final int REQUEST_CODE_SELECT_FOLDER = 3;
+    private static final int REQUEST_CODE_GET_FOLDER_NAME = 4;
 
+    private static int nCurrentRequest = 0;
 
-    private static final int REQUEST_CODE_OPENER = 3;
+    private ResultCallback<DriveResource.MetadataResult> mMetaResult = null;
 
 
 
@@ -71,15 +82,18 @@ public class MainActivity extends AppCompatActivity implements
 
         setContentView(R.layout.activity_main);
 
-
-
         mBtnStart = (Button)findViewById(R.id.btn_start);
         mBtnSelectGoolgeDrive = (Button)findViewById(R.id.btn_select_google_drive);
-
+        mTvSelectedFolderName = (TextView)findViewById(R.id.tv_selected_foldername);
 
         mSelectGoogleDriveFolderId = SharedPrefUtil.getDriveFolderInfo(mCtx);
 
         Log.i(TAG, "mSelectGoogleDriveFolderId:::" + mSelectGoogleDriveFolderId);
+
+        if(SharedPrefUtil.getDriveFolderInfo(mCtx) != null
+                && SharedPrefUtil.getDriveFolderInfo(mCtx).length() > 0 ){
+            connectGoogleDriveApi(REQUEST_CODE_GET_FOLDER_NAME);
+        }
 
         mBtnStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,41 +104,62 @@ public class MainActivity extends AppCompatActivity implements
             }
         });
 
+        mMetaResult = new ResultCallback<DriveResource.MetadataResult>() {
+            @Override
+            public void onResult(@NonNull DriveResource.MetadataResult metadataResult) {
+                if(metadataResult.getStatus().isSuccess() != true){
+                    Log.i(TAG, "fail to retrieve");
+                    return;
+                }
+
+                Metadata metadata = metadataResult.getMetadata();
+                mTvSelectedFolderName.setText(metadata.getTitle());
+            }
+        };
+
         Log.i(TAG, "why?? so serious???");
         mBtnSelectGoolgeDrive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (mGoogleApiClient == null) {
-                    mGoogleApiClient = new GoogleApiClient.Builder(mCtx)
-                            .addApi(Drive.API)
-                            .addScope(Drive.SCOPE_FILE)
-                            .addScope(Drive.SCOPE_APPFOLDER) // required for App Folder sample
-                            .addConnectionCallbacks(mThis)
-                            .addOnConnectionFailedListener(mThis)
-                            .build();
-                }
-
-                Log.i(TAG, "connect request!!!");
-                if(mGoogleApiClient.isConnected() == true){
-                    IntentSender intentSender = Drive.DriveApi
-                            .newOpenFileActivityBuilder()
-                            .setMimeType(new String[] { DriveFolder.MIME_TYPE })
-                            .build(mGoogleApiClient);
-                    try {
-                        startIntentSenderForResult(
-                                intentSender, REQUEST_CODE_OPENER, null, 0, 0, 0);
-                    } catch (IntentSender.SendIntentException e) {
-                        Log.w(TAG, "Unable to send intent", e);
-                    }
-
-                }
-                else{
-                    mGoogleApiClient.connect();
-                }
+                connectGoogleDriveApi(REQUEST_CODE_SELECT_FOLDER);
 
             }
         });
+    }
+
+    protected void connectGoogleDriveApi(int nRequestCode){
+
+        nCurrentRequest = nRequestCode;
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(mCtx)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addScope(Drive.SCOPE_APPFOLDER) // required for App Folder sample
+                    .addConnectionCallbacks(mThis)
+                    .addOnConnectionFailedListener(mThis)
+                    .build();
+        }
+
+        Log.i(TAG, "connect request!!!");
+        if(mGoogleApiClient.isConnected() == true
+                && nCurrentRequest == REQUEST_CODE_SELECT_FOLDER
+                ){
+            IntentSender intentSender = Drive.DriveApi
+                    .newOpenFileActivityBuilder()
+                    .setMimeType(new String[] { DriveFolder.MIME_TYPE })
+                    .build(mGoogleApiClient);
+            try {
+                startIntentSenderForResult(
+                        intentSender, nRequestCode, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                Log.w(TAG, "Unable to send intent", e);
+            }
+
+        }
+        else{
+            mGoogleApiClient.connect();
+        }
     }
     /**
      * Handles resolution callbacks.
@@ -137,16 +172,28 @@ public class MainActivity extends AppCompatActivity implements
             mGoogleApiClient.connect();
         }
 
-        if ( requestCode == REQUEST_CODE_OPENER){
+        if ( requestCode == REQUEST_CODE_SELECT_FOLDER){
             if (resultCode == RESULT_OK) {
                 DriveId driveId = (DriveId) data.getParcelableExtra(
                         OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
 
-                SharedPrefUtil.setDriveFolderInfo(driveId.getResourceId() , mCtx);
+                SharedPrefUtil.setDriveFolderInfo(driveId.toString() , mCtx);
                 Log.i("pick", "driveId::" +driveId);
-            }
+                DriveFile file = Drive.DriveApi.getFile(mGoogleApiClient, DriveId.decodeFromString(driveId.toString()));
+                file.getMetadata(mGoogleApiClient).setResultCallback(mMetaResult);
 
+            }
         }
+
+//        if( requestCode == REQUEST_CODE_GET_FOLDER_NAME){
+//            if(resultCode == RESULT_OK){
+//                DriveId driveId = (DriveId) data.getParcelableExtra(
+//                        OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
+//
+//                DriveFile file = Drive.DriveApi.getFile(mGoogleApiClient, DriveId.decodeFromString(driveId.toString()));
+//                file.getMetadata(mGoogleApiClient).setResultCallback(mMetaResult);
+//            }
+//        }
 
     }
 
@@ -158,16 +205,25 @@ public class MainActivity extends AppCompatActivity implements
         EXISTING_FOLDER_ID = Drive.DriveApi.getRootFolder(mGoogleApiClient).getDriveId().getResourceId();
         Log.i(TAG, "EXISTING_FOLDER_ID:::" + EXISTING_FOLDER_ID);
 
-        IntentSender intentSender = Drive.DriveApi
-                .newOpenFileActivityBuilder()
-                .setMimeType(new String[] { DriveFolder.MIME_TYPE })
-                .build(mGoogleApiClient);
-        try {
-            startIntentSenderForResult(
-                    intentSender, REQUEST_CODE_OPENER, null, 0, 0, 0);
-        } catch (IntentSender.SendIntentException e) {
-            Log.w(TAG, "Unable to send intent", e);
+        if(nCurrentRequest == REQUEST_CODE_SELECT_FOLDER){
+            IntentSender intentSender = Drive.DriveApi
+                    .newOpenFileActivityBuilder()
+                    .setMimeType(new String[] { DriveFolder.MIME_TYPE })
+                    .build(mGoogleApiClient);
+            try {
+                startIntentSenderForResult(
+                        intentSender, REQUEST_CODE_SELECT_FOLDER, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                Log.w(TAG, "Unable to send intent", e);
+            }
         }
+
+        if(nCurrentRequest == REQUEST_CODE_GET_FOLDER_NAME){
+
+//            DriveFile file = Drive.DriveApi.getFile(mGoogleApiClient, DriveId.decodeFromString(SharedPrefUtil.getDriveFolderInfo(mCtx)));
+//            file.getMetadata(mGoogleApiClient).setResultCallback(mMetaResult);
+        }
+
     }
 
     @Override
@@ -191,4 +247,5 @@ public class MainActivity extends AppCompatActivity implements
         }
 
     }
+
 }
